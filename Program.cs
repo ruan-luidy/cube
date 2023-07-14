b77496e8e607335454db6d2f74716b06d81ecb5b
ruan luidy <ruan.liidy23@gmail.com>
2023-07-14 16:43:19 -0300

    sobrou

--- Program.cs
+++ Program.cs
@@ -4,9 +4,9 @@ class Program
 {
     static float A, B, C;
     static float cubeWidth = 20;
-    static int width = 160, heigth = 44;
-    static float[] zBuffer = new float[width * heigth];
-    static char[] buffer = new char[width * heigth];
+    static int width = 160, height = 44;
+    static float[] zBuffer = new float[width * height];
+    static char[] buffer = new char[width * height];
     static int backgroundASCIICode = '.';
     static int distanceFromCam = 100;
     static float horizontalOffset;
@@ -17,37 +17,37 @@ class Program
     static int xp, yp;
     static int idx;
 
-    static float CalculateX(int i, int j, int k)
+    static float CalculateX(float i, float j, float k)
     {
         return j * (float)Math.Sin(A) * (float)Math.Sin(B) * (float)Math.Cos(C) - k * (float)Math.Cos(A) * (float)Math.Sin(B) * (float)Math.Cos(C) +
             j * (float)Math.Cos(A) * (float)Math.Sin(C) + k * (float)Math.Sin(A) * (float)Math.Sin(C) + i * (float)Math.Cos(B) * (float)Math.Cos(C);
     }
 
-    static float CalculateY(int i, int j, int k)
+    static float CalculateY(float i, float j, float k)
     {
         return j * (float)Math.Cos(A) * (float)Math.Cos(C) + k * (float)Math.Sin(A) * (float)Math.Cos(C) -
             j * (float)Math.Sin(A) * (float)Math.Sin(B) * (float)Math.Sin(C) + k * (float)Math.Cos(A) * (float)Math.Sin(B) * (float)Math.Sin(C) -
             i * (float)Math.Cos(B) * (float)Math.Sin(C);
     }
 
-    static float CalculateZ(int i, int j, int k)
+    static float CalculateZ(float i, float j, float k)
     {
         return k * (float)Math.Cos(A) * (float)Math.Cos(B) - j * (float)Math.Sin(A) * (float)Math.Cos(B) + i * (float)Math.Sin(B);
     }
 
     static void CalculateForSurface(float cubeX, float cubeY, float cubeZ, char ch)
     {
-        x = CalculateX((int)cubeX, (int)cubeY, (int)cubeZ);
-        y = CalculateY((int)cubeX, (int)cubeY, (int)cubeZ);
-        z = CalculateZ((int)cubeX, (int)cubeY, (int)cubeZ) + distanceFromCam;
+        x = CalculateX(cubeX, cubeY, cubeZ);
+        y = CalculateY(cubeX, cubeY, cubeZ);
+        z = CalculateZ(cubeX, cubeY, cubeZ) + distanceFromCam;
 
         ooz = 1 / z;
 
         xp = (int)(width / 2 + horizontalOffset + K1 * ooz * x * 2);
-        yp = (int)(heigth / 2 + K1 * ooz * z);
+        yp = (int)(height / 2 + K1 * ooz * y);
 
         idx = xp + yp * width;
-        if (idx >= 0 && idx < width * heigth)
+        if (idx >= 0 && idx < width * height)
         {
             if (ooz > zBuffer[idx])
             {
@@ -62,8 +62,8 @@ class Program
         Console.Clear();
         while (true)
         {
-            Array.Fill(buffer, backgroundASCIICode);
-            Array.Fill(zBuffer, 0);
+            Array.Fill<char>(buffer, backgroundASCIICode);
+            Array.Fill<float>(zBuffer, 0);
 
             cubeWidth = 20;
             horizontalOffset = -2 * cubeWidth;
@@ -82,10 +82,10 @@ class Program
             }
 
             Console.SetCursorPosition(0, 0);
-            for (int k = 0; k < width * heigth; k++)
+            for (int k = 0; k < width * height; k++)
             {
-                Console.WriteLine(buffer[k]);
-                if (k % width == width - 1)
+                Console.Write(buffer[k]);
+                if ((k + 1) % width == 0)
                     Console.WriteLine();
             }
 
