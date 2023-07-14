ae1b18a5f37db9f8f58c328e347122fee05a6746
ruan.melo <ruan.melo@PCP08>
2023-07-14 10:23:28 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static float A, B, C;
+    static float cubWidth = 20;
+    static int width = 160, heigth = 44;
+    static float[] zBuffer = new float[width * heigth];
+    static char[] buffer = new char[width * heigth];
+    static int backgroundASCIICode = '.';
+    static int distanceFromCam = 100;
+    static float horizontalOffSet;
+    static float K1 = 40;
+    static float incrementSpeed = 0.6f;
+    static float x, y, z;
+    static float ooz;
+    static int xp, yp;
+    static int idx;
+}
